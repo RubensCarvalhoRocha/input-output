d5e88c25b2b732381d9deec53b44892b503f2987
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-04-23 14:53:34 -0300

    creatiion of the carList with mock data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Car } from './car';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'input-output';
+  savedCarList: Car[] = [];
+  carList: Car[] = [
+    {
+      make: 'Foyoda',
+      model: 'Famery',
+      miles: 54354,
+      price: 1000,
+      year: 2022,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Ronda',
+      model: 'Disaccord',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Specific Motors',
+      model: 'Spoke',
+      miles: 100000,
+      price: 230,
+      year: 1991,
+      transmission: 'Automatic',
+    },
+    {
+      make: 'Fjord',
+      model: 'Pocus',
+      miles: 1,
+      price: 22330,
+      year: 2023,
+      transmission: 'Automatic',
+    },
+  ];
 }
